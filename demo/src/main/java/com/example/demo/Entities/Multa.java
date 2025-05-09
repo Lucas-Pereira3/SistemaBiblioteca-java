fd13b8361f6412ed840550a82568e9f8e13d1b36
Jhonatan de Souza <168601314+Jhonatan-S-Silva@users.noreply.github.com>
2025-05-08 22:18:35 -0300

    entidade, repositorio, enum = multa

---
+++ demo/src/main/java/com/example/demo/Entities/Multa.java
@@ -0,0 +1,46 @@
+package com.example.demo.Entities;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "Multa")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+
+public class Multa {
+
+       @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "emprestimo_id")
+    private Long empregoId;
+
+    @Column(name = "valor", precision  = 18, scale = 2)
+    private BigDecimal valor;
+
+    @Column(name = "status", length = 20, nullable = false)
+    private StatusMulta status;
+
+    @Column(name = "data_pagamento")
+    private LocalDateTime dataPagamento;
+
+}
